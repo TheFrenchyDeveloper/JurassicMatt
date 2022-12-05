c3072873c3f6b5bf1e2132a6d3a660ea13b9e63e
TheFrenchyDeveloper <kevin.massol@icloud.com>
2022-12-05 17:03:12 +0100

    [FEAT] - Added assets and renamed "ContentView" file to "HomeView"

---
+++ JurassicMatt/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JurassicMatt
+//
+//  Created by KEVIN on 03/12/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ JurassicMatt/JurassicMattApp.swift
@@ -0,0 +1,17 @@
+//
+//  JurassicMattApp.swift
+//  JurassicMatt
+//
+//  Created by KEVIN on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct JurassicMattApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
